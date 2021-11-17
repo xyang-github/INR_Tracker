f233be509f4e8bb91afa29659cf0c6c3ce6c8ef3
xyang-github <x.yang89@yahoo.com>
2021-11-17 11:04:18 -0800

    Updated main uic file and decoupled GUI from logic

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-import re
-from main_ui import Ui_MainWindow
 import sys
-import sqlite3
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import *
+from main_ui import *
 
 
-con = sqlite3.connect('inr_database.db')
-
-
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+class DlgMain(QMainWindow, Ui_dlgMain):
+    def __init__(self):
+        super(DlgMain, self).__init__()
         self.setupUi(self)
-        self.pushEnter.clicked.connect(self.search_patient)
-
-    def validate_mrn(self, candidate):
-        """
-        :param candidate: the value typed into the MRN textbox
-        :return: a message box if invalid entry, or no matching record found
-        """
-        if re.match("[^0-9]+", candidate):
-            QMessageBox.warning(None, "Error", "Only enter the numeric medical record number.")
-
-        return candidate
-
-    def search_patient(self):
-        """
-        Takes a valid medical record number, and select a matching entry from the database
-        """
-        patient_mrn = int(self.validate_mrn(self.textMRN.text()))
-        cur = con.cursor()
-        cur.execute("select * from patient where patient_id = ?;", (patient_mrn,))
-        print(cur.fetchone())
 
 
 if __name__ == "__main__":
-    myapp = QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
-    sys.exit(myapp.exec_())
+    app = QApplication(sys.argv)
+    dlgMain = DlgMain()
+    dlgMain.show()
+    sys.exit(app.exec_())
