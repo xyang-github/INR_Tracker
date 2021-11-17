e63568e390f2de79c1e62eb41cfb45455467f476
xyang-github <x.yang89@yahoo.com>
2021-11-17 11:10:20 -0800

    Created exit signal and slot

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ class DlgMain(QMainWindow, Ui_dlgMain):
         super(DlgMain, self).__init__()
         self.setupUi(self)
 
+        self.actionExit.triggered.connect(self.evt_actionExit_triggered)  # exit signal
+
+    def evt_actionExit_triggered(self):
+        """
+        Exit the program when user clicks on File > Exit
+        """
+        sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
